8da443b67b6a5846e21bad5ff3b4c5f9339a080f
kshitijkumarsaini <9968732560kshitijkumar@gmail.com>
2021-06-06 19:36:18 +0530

    Added redux to Product Details Page

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,26 @@ const data = require('./data');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.get('/api/products', (req, res, next) => {
     res.send(data.products);
 });
 
+
+app.get('/api/product/:id', (req, res, next) => {
+    const product = data.products.find((x) => x._id === req.params.id);
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product Not Found' })
+    }
+});
+
+
 app.get('/', (req, res, next) => {
     res.send('Server is Ready');
 });
+
 
 app.listen(PORT, () => {
     console.log('Serve at host 5000');
