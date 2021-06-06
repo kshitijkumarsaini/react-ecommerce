c36b354c0024a1a453d67a678492143dda43c21d
kshitijkumarsaini <9968732560kshitijkumar@gmail.com>
2021-06-06 15:32:52 +0530

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,32 @@
 import React from 'react';
-import data from './data';
-import Product from './components/Product';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">React Ecommerce</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-
-      <main>
-        <div>
-          <div className="row center">
-            {
-              data.products.map(product => (
-                <Product key={product._id} product={product} />
-              ))
-            }
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">React Ecommerce</a>
           </div>
-        </div>
-      </main>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
 
-      <footer className="row center">
-        All Rights Reserved ©Copyright 2021.
-        </footer>
-    </div>
+        <main>
+        <Route path="/" component={HomeScreen} exact></Route>
+        <Route path="/product/:id" component={ProductScreen}></Route>
+        </main>
+
+        <footer className="row center">
+          All Rights Reserved ©Copyright 2021.
+          </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
