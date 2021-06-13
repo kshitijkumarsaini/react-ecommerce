b57ee9e0e4340d356ff96abfe15a5f34840a97d6
kshitijkumarsaini <9968732560kshitijkumar@gmail.com>
2021-06-13 18:31:47 +0530

    Created shopping cart to show products

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import MessageBox from "../components/MessageBox";
 import { addToCart } from "../actions/cartActions";
 
 export default function CartScreen(props) {
@@ -7,6 +9,9 @@ export default function CartScreen(props) {
   const qty = props.location.search
     ? Number(props.location.search.split("=")[1])
     : 1;
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -15,12 +20,91 @@ export default function CartScreen(props) {
     }
   }, [dispatch, productId, qty]);
 
+  const removeFromCartHandler = (id) => {
+    // delete the item from the product
+  };
+
+  const checkoutHandler = () => {
+    props.history.push("/signin?redirect=shipping");
+  };
   return (
-    <div>
-      <h1>Cart Screen</h1>
-      <p>
-        Add To Cart : ProductID: {productId} Qty: {qty}
-      </p>
+    <div className="row top">
+      <div className="col-2">
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <MessageBox>
+            Cart is Empty. <Link to="/">Go Shopping</Link>
+          </MessageBox>
+        ) : (
+          <ul>
+            {cartItems.map((item) => (
+              <li key={item.product}>
+                <div className="row">
+                  <div>
+                    <img
+                      src={item.image}
+                      alt={item.name}
+                      className="small"
+                    ></img>
+                  </div>
+                  <div className="min-30">
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </div>
+                  <div>
+                    <select
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {
+                        [...Array(item.countInStock).keys()].map((item) => (
+                          <option key={item + 1} value={item + 1}>
+                            {item + 1}
+                          </option>
+                        )) // Returns a list of options based on the value of countInStock
+                      }
+                    </select>
+                  </div>
+                  <div>${item.price}</div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="col-1">
+        <div className="card card-body">
+          <ul>
+            <li>
+              <h2>
+                Subtotal ({cartItems.reduce((a, c) => a + c.qty, 0)} items) : $
+                {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+              </h2>
+            </li>
+            <li>
+              <button
+                className="primary block"
+                type="button"
+                onClick={checkoutHandler}
+                disabled={cartItems.length === 0}
+              >
+                Proceed To Checkout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from "react";
 // import axios from 'axios';
-import { useDispatch, useSelector } from 'react-redux';
-import Product from '../components/Product';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox'
-import { listProducts } from '../actions/productActions';
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { listProducts } from "../actions/productActions";
 
 export default function HomeScreen() {
-    // const [products, setProducts] = useState([]);
-    // const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState(false);
-    const dispatch = useDispatch();
+  // const [products, setProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  const dispatch = useDispatch();
 
-    const productList = useSelector((state) => state.productList);
-    const { loading, error, products } = productList;
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-    useEffect(() => {
-        // const fetchData = async () => {
-        //     try {
-        //         setLoading(true);
-        //         const { data } = await axios.get('/api/products');
-        //         setLoading(false);
-        //         setProducts(data);
-        //     } catch (err) {
-        //         setError(err.message);
-        //         setLoading(false);
-        //     }
-        // };
-        // fetchData();
-        dispatch(listProducts());
-    }, [dispatch]);
+  useEffect(() => {
+    // const fetchData = async () => {
+    //     try {
+    //         setLoading(true);
+    //         const { data } = await axios.get('/api/products');
+    //         setLoading(false);
+    //         setProducts(data);
+    //     } catch (err) {
+    //         setError(err.message);
+    //         setLoading(false);
+    //     }
+    // };
+    // fetchData();
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (
-        <div>
-            {loading ? (
-                <LoadingBox></LoadingBox>
-            ) : error ? (
-                <MessageBox variant="danger" >{error}</MessageBox>
-            ) : (
-                <div className="row center">
-                    {products.map(product => (
-                        <Product key={product._id} product={product} />
-                    ))}
-                </div>
-            )}
+  return (
+    <div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
         </div>
-    )
+      )}
+    </div>
+  );
 }
